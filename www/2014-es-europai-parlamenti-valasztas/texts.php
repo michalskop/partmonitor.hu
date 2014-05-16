b4f1fc159c3e5bf205e8d027e75b309679a89807
Michal Skop <michal.skop@kohovolit.eu>
2014-05-16 13:38:42 +0200

    hu texts

--- www/2014-es-europai-parlamenti-valasztas/texts.php
+++ www/2014-es-europai-parlamenti-valasztas/texts.php
@@ -9,86 +9,75 @@ $text = array(
   //meta
   'author' => 'KohoVolit.eu',
   'description' => 'Euro-elections 2014 are coming!',
-  'keywords' => 'elections 2014, european elections, election calculator, election test',
+  'keywords' => '2014-es választások, európai választások, választási kalkulátor, választási teszt',
   //fb
   'og:image' => 'http://volebnikalkulacka.cz/image/logo.jpg',
-  //'og:title' => 'Volební kalkulačka',
-  //'og:url' => 'http://volebnikalkulacka.cz',
-  //'og:site_name' => 'Volební kalkulačka',
   'author_link' => 'http://kohovolit.eu',
   'licence' => 'Creative Commons BY 4.0',
   'year' => '2014',
   
-  'footer_text' => 'Election Calculators by <a href="http://kohovolit.eu">KohoVolit.eu</a>. Since 2006.',
+  'footer_text' => 'Választási kalkulátorok a <a href="http://kohovolit.eu">KohoVolit.eu</a>-tól. 2006 óta.',
   
 
-  'calc' => 'Election Calculator',
-  'info' => 'Info',
-  'url' => 'http://volebnikalkulacka.cz', 
-  'title' => 'Election Calculator 2014 | European Parliament' ,
+  'calc' => 'Voksmonitor',
+  'info' => 'Infó',
+  'url' => 'http://voksmonitor.hu', 
+  'title' => 'Voksmonitor 2014 | Európai Parlament',
 
    //PAGE
-  'calc_name' => 'Election Calculator - Euro-elections 2014 (Czech parties)',
-  'page_question_yes' => 'YES',
-  'page_question_no' => 'NO',
-  'page_question_neutral' => 'I do not know',
+  'calc_name' => 'Voksmonitor - európai parlamenti választások 2014 (magyar pártok)',
+  'page_question_yes' => 'IGEN',
+  'page_question_no' => 'NEM',
+  'page_question_neutral' => 'Nem tudom',
   'page_question_neutral_add' => '',
-  'page_important_questions' => 'Important Questions', 
-  'page_important_legend' => '<strong>Select the most important questions for you</strong>.  You will get better results. This step is optional, you may skip it.',
-  'page_important_skip' => 'Skip this step',
-  'page_important_results' => 'Results',  
+  'page_important_questions' => 'Fontos kérdés', 
+  'page_important_legend' => '<strong>Válaszd ki a számodra legfontosabb kérdéseket</strong>.  Így pontosabb lesz az eredmény. Ez a rész opcionális, kihagyhatod.',
+  'page_important_skip' => 'Kihagyom',
+  'page_important_results' => 'Eredmények',  
   
-  'ie8' => 'We are sorry, but Election Calculator may not work in Internet Explorer 8 and older.',
-  'opera11' =>'We are sorry, but Election Calculator may not work in Opera 11 and older',
+  'ie8' => 'A Voksmonitor Internet Explorer 8-ban és annál régebbi verziókban lehet, hogy nem jelenik meg megfelelõen.',
+  'opera11' =>'A Voksmonitor Opera 11-ben és annál régebbi verziókban lehet, hogy nem jelenik meg megfelelõen.',
 
    //MATCH
-   'match_save' => 'Save your votes',
+   'match_save' => 'Szavazataid mentése',
  
-   'result_winner_share' => 'Share results on Facebook',    
-   'result_share' => 'Share results',
+   'result_winner_share' => 'Oszd meg az eredményt Facebookon',    
+   'result_share' => 'Eredmények megosztása',
    
-   'comparison_parties_eu' => 'Comparison with parties in EU',
-   'comparison_eu_explained' => '15 questions are common for several EU calculators',
-   'missing_table_description' => 'Following parties <strong>have not answered the questions yet</strong>: ',
+   'comparison_parties_eu' => 'Összehasonlítás EU-s pártokkal',
+   'comparison_eu_explained' => '15 kérdés azonos több EU-s ország Voksmonitorában',
+   'missing_table_description' => 'Az alábbi pártok <strong> még nem válaszolták meg a kérdéseket</strong>: ',
    
    //fb
-   'result_fb_appid' => "200785490055088",
-  'result_fb_picture_link' => 'http://volebnakalkulacka.sk/europsky-parlament-2014/image/logo/',
-  'result_fb_name' => 'Czech VAA:',
-  'result_fb_description_1' => 'My closest Czech party: ',
-  'result_fb_description_match' => 'match',
-  'result_fb_description_and' => 'and',
-  'result_fb_description_only' => 'only',
-  'result_fb_description_2' => 'Least ',
-  'result_fb_description_3' => 'Find out who is your best match on VolebniKalkulacka.cz',
-  'result_fb_redirect_link' => 'http://volebnikalkulacka.cz/',
-  'result_fb_link' => 'http://volebnikalkulacka.cz/',
-  'result_fb_action' => "[{'name':'Who to vote for?','link':'http://volebnikalkulacka.cz/'}]",
+   'result_fb_appid' => "297106463778572",
+  'result_fb_picture_link' => 'http://voksmonitor.hu/european-parliament-2014/image/logo/',
+  'result_fb_name' => 'EP Voksmonitor:',
+  'result_fb_description_1' => 'Az alábbi magyar pártok programja áll hozzám legközelebb: ',
+  'result_fb_description_match' => 'egyezés',
+  'result_fb_description_and' => 'és',
+  'result_fb_description_only' => 'csak',
+  'result_fb_description_2' => 'A legtávolabb pedig: ',
+  'result_fb_description_3' => 'Nézd meg, melyik párt programja áll hozzád a legközelebb: Voksmonitor.hu',
+  'result_fb_redirect_link' => 'http://voksmonitor.hu/',
+  'result_fb_link' => 'http://voksmonitor.hu/',
+  'result_fb_action' => "[{'name':'Kire szavazzak?','link':'http://voksmonitor.hu/'}]",
    
    //tabs
-   'result_results' => 'Results',
-   'result_comparison' => 'Comparison',
-   'result_eu' => 'Results in EU',
-   
-   //support (DMS)
-   'result_support' => '<strong>Support “Electoral calculator” to keep it independent</strong><br/>
-  Send a text message to &nbsp;number&nbsp;<strong><span class="colorstrong">87777</span></strong>
-  with&nbsp;text <strong><span class="colorstrong">DMS&nbsp;KOHOVOLIT</span></strong>
-  (charge 30Kč, out of which 28Kč for KohoVolit.eu) or 
-   <strong><span class="colorstrong">DMS&nbsp;ROK&nbsp;KOHOVOLIT</span></strong>
-   (automatically every month for 1 year)',
+   'result_results' => 'Eredmények',
+   'result_comparison' => 'Összehasonlítás',
+   'result_eu' => 'EU-s eredmények',
    
    //comparison
-   'result_close' => 'Close',
-   'result_yes' => 'Yes',
-   'result_no' => 'No',
-   'result_question' => 'Question',
+   'result_close' => 'Közel áll',
+   'result_yes' => 'Igen',
+   'result_no' => 'Nem',
+   'result_question' => 'Kérdés',
    
    //save
-   'result_save_description' => 'You may return to your results any time, just copy this whole link:',
+   'result_save_description' => 'Bármikor újra megnézheted az eredményeidet, csak másold ki ezt a linket:',
 
    //google analytics tracking id
-   'google_tracking_id' => 'UA-8592359-7',   
+   'google_tracking_id' => 'UA-49538088-1',   
 
   //question
 
@@ -97,14 +86,15 @@ $text = array(
   //RESULTS
 
   'result_twitter_data_lang' => 'en',
-  'result_twitter_text_1' => 'Euro-election calculator:1st ',
+  'result_twitter_text_1' => 'EP választás kalkulátor: elsõ ',
   //'result_twitter_text_2' => '1.',
-  //'result_twitter_and' => 'a',
-  'result_twitter_text_3' => ', last ',
+  //'result_twitter_and' => 'és',
+  'result_twitter_text_3' => ', utolsó ',
   'result_twitter_account' => 'KohoVolitEU',
-  'result_twitter_hashtags' => 'euroelections',
+  'result_twitter_hashtags' => 'voksmonitor',
   'result_twitter_share' => 'Tweet',
 
   
   
 );
+
