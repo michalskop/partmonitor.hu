1c19ee3d1d6067ef5cb27d03e094e007b14a3f18
Michal Skop <michal.skop@kohovolit.eu>
2014-10-06 01:06:59 +0200

    budapest alpha

---
+++ smarty/templates/2014-es-onkormanyzati-valasztas/match-tabs.tpl
@@ -0,0 +1,12 @@
+<!-- Nav tabs -->
+<ul class="nav nav-tabs">
+  <li class="active"><a href="#home" data-toggle="tab" id="tabs-home"><i class="fa fa-list-ol"></i> {$text['result_results']}</a></li>
+  <li><a href="#comparison" data-toggle="tab" id="tabs-comparison"><strong><i class="fa fa-th"></i> {$text['result_comparison']}</strong></a></li>
+ <!--<li><a href="#euro" data-toggle="tab" id="tabs-euro"><i class="fa fa-flag-o"></i> {$text['result_eu']}</a></li>-->
+</ul>
+
+<!-- Tab panes -->
+<div class="tab-content"> 
+  {include "match-table.tpl"}
+  {include "match-matrix.tpl"}
+</div> <!-- /.tab-content -->
---
+++ www/2014-es-onkormanyzati-valasztas/texts.php
@@ -0,0 +1,101 @@
+<?php
+/**
+* Texts (for translation)
+*/
+$text = array(
+  
+  'election_code' => '2014-es-onkormanyzati-valasztas',
+  'template_code' => '2014-es-onkormanyzati-valasztas',
+  'lang' => 'hu',
+  //meta
+  'author' => 'KohoVolit.eu',
+  'description' => 'Euro-elections 2014 are coming!',
+  'keywords' => '2014-es választások, európai választások, választási kalkulátor, választási teszt',
+  //fb
+  'og:image' => 'http://volebnikalkulacka.cz/image/logo.jpg',
+  'author_link' => 'http://kohovolit.eu',
+  'licence' => 'Creative Commons BY 4.0',
+  'year' => '2014',
+  
+  'footer_text' => 'Választási kalkulátorok a <a href="http://kohovolit.eu">KohoVolit.eu</a>-tól. 2006 óta.',
+  
+
+  'calc' => 'Voksmonitor',
+  'info' => 'Infó',
+  'url' => 'http://voksmonitor.hu', 
+  'title' => 'Voksmonitor 2014 | Európai Parlament',
+
+   //PAGE
+  'calc_name' => 'Voksmonitor - európai parlamenti választások 2014 (magyar pártok)',
+  'page_question_yes' => 'Igen, egyetértek',
+  'page_question_no' => 'Nem értek egyet',
+  'page_question_neutral' => 'Nem tudom',
+  'page_question_neutral_add' => '',
+  'page_important_questions' => 'Fontos kérdés', 
+  'page_important_legend' => '<strong>Válaszd ki a számodra legfontosabb kérdéseket</strong>.  Így pontosabb lesz az eredmény. Ez a rész opcionális, kihagyhatod.',
+  'page_important_skip' => 'Kihagyom',
+  'page_important_results' => 'Eredmények',  
+  
+  'ie8' => 'A Voksmonitor Internet Explorer 8-ban és annál régebbi verziókban lehet, hogy nem jelenik meg megfelelõen.',
+  'opera11' =>'A Voksmonitor Opera 11-ben és annál régebbi verziókban lehet, hogy nem jelenik meg megfelelõen.',
+
+   //MATCH
+   'match_save' => 'Szavazataid mentése',
+ 
+   'result_winner_share' => 'Oszd meg az eredményt Facebookon',    
+   'result_share' => 'Eredmények megosztása',
+   
+   'comparison_parties_eu' => 'Összehasonlítás EU-s pártokkal',
+   'comparison_eu_explained' => '15 kérdés azonos több EU-s ország Voksmonitorában',
+   'missing_table_description' => 'Az alábbi pártok <strong> még nem válaszolták meg a kérdéseket</strong>: ',
+   
+   //fb
+   'result_fb_appid' => "297106463778572",
+  'result_fb_picture_link' => 'http://voksmonitor.hu/european-parliament-2014/image/logo/',
+  'result_fb_name' => 'EP Voksmonitor:',
+  'result_fb_description_1' => 'Az alábbi magyar pártok programja áll hozzám legközelebb: ',
+  'result_fb_description_match' => 'egyezés',
+  'result_fb_description_and' => 'és',
+  'result_fb_description_only' => 'csak',
+  'result_fb_description_2' => 'A legtávolabb pedig: ',
+  'result_fb_description_3' => 'Nézd meg, melyik párt programja áll hozzád a legközelebb: Voksmonitor.hu',
+  'result_fb_redirect_link' => 'http://voksmonitor.hu/',
+  'result_fb_link' => 'http://voksmonitor.hu/',
+  'result_fb_action' => "[{'name':'Kire szavazzak?','link':'http://voksmonitor.hu/'}]",
+   
+   //tabs
+   'result_results' => 'Eredmények',
+   'result_comparison' => 'Összehasonlítás',
+   'result_eu' => 'EU-s eredmények',
+   
+   //comparison
+   'result_close' => 'Bezár',
+   'result_yes' => 'Igen',
+   'result_no' => 'Nem',
+   'result_question' => 'Kérdés',
+   
+   //save
+   'result_save_description' => 'Bármikor újra megnézheted az eredményeidet, csak másold ki ezt a linket:',
+
+   //google analytics tracking id
+   'google_tracking_id' => 'UA-49538088-1',   
+
+  //question
+
+  //important questions 
+  
+  //RESULTS
+
+  'result_twitter_data_lang' => 'en',
+  'result_twitter_text_1' => 'EP választás kalkulátor: elsõ ',
+  //'result_twitter_text_2' => '1.',
+  //'result_twitter_and' => 'és',
+  'result_twitter_text_3' => ', utolsó ',
+  'result_twitter_account' => 'KohoVolitEU',
+  'result_twitter_hashtags' => 'voksmonitor',
+  'result_twitter_share' => 'Tweet',
+
+  
+  
+);
+
