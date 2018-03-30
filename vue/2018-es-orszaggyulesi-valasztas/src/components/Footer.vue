dbecd65eb7eff29aa01713440e938da5bf6fc4d0
Michal Skop <michal.skop@kohovolit.eu>
2018-03-30 17:07:20 +0200

    voksmonitor.hu 2

--- vue/2018-es-orszaggyulesi-valasztas/src/components/Footer.vue
+++ vue/2018-es-orszaggyulesi-valasztas/src/components/Footer.vue
@@ -6,11 +6,11 @@
 
                 </div>
                 <div class="col-md-6 text-center">
-                    <a href="http://kmonitor.hu" title="K-monitor" id="footer-kmonitor-logo"><img :src="createImageLink('statics/pictures/kmonitor.png')" alt="K-Monitor"/></a><br />
-                    <a href="http://kohovolit.eu" title="KohoVolit.eu" id="footer-kohovolit-logo"><img src="https://volebnikalkulacka.cz/image/logo.png" alt="logo kohovolit.eu"></a>
+                    <a href="http://kmonitor.hu" title="K-monitor" id="footer-kmonitor-logo"><img :src="createImageLink('statics/pictures/kmonitor.png')" alt="K-Monitor"/></a><br /><br />
+                    <a href="http://kohovolit.eu" title="KohoVolit.eu" id="footer-kohovolit-logo"><img src="https://volebnikalkulacka.cz/image/logo.png" alt="logo kohovolit.eu"></a><br />
 					<p class="lead black">Választási kalkulátorok a <a href="http://kohovolit.eu">KohoVolit.eu</a>-tól. 2006 óta.</p>
                     <hr>
-					<p class="black">Creative Commons BY-SA 4.0 | <a href="http://kohovolit.eu" id="footer-kohovolit-licence">KohoVolit.eu</a> & <a href="http://kmonitor.hu">K-monitor</a> 2018</p>
+					<p class="black">Creative Commons BY-SA 4.0 | <a href="http://kohovolit.eu" id="footer-kohovolit-licence">KohoVolit.eu</a> & <a href="http://kmonitor.hu">K-Monitor</a> 2018</p>
                 </div>
             </div>
         </div>
