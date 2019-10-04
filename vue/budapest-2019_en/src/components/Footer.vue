ee77ac70d7a39772c32e9f6f9ed4e0ff0a62a25b
Michal Skop <michal.skop@kohovolit.eu>
2019-10-04 22:12:34 +0200

    english

---
+++ vue/budapest-2019_en/src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+    <footer class="text-muted mt-4 md-4">
+        <div class="container">
+            <div class="row justify-center">
+                <div class="col-md-3">
+
+                </div>
+                <div class="col-md-6 text-center">
+                    <a href="http://kmonitor.hu" title="K-monitor" id="footer-kmonitor-logo"><img src="https://voksmonitor.azureedge.net/hu/2018-es-orszaggyulesi-valasztas/statics/pictures/kmonitor.png" alt="K-Monitor"/></a><br /><br />
+                    <a href="http://kohovolit.eu" title="KohoVolit.eu" id="footer-kohovolit-logo"><img src="https://volebnikalkulacka.cz/image/logo.png" alt="logo kohovolit.eu"></a><br />
+                    <p class="lead black">Választási kalkulátorok a <a href="http://kohovolit.eu">KohoVolit.eu</a>-tól. 2006 óta.</p>
+                    <hr>
+					<p class="black">Creative Commons BY-SA 4.0 | <a href="http://kohovolit.eu" id="footer-kohovolit-licence">KohoVolit.eu</a> & <a href="http://kmonitor.hu">K-Monitor</a> 2019</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+
+</script>
---
+++ vue/budapest-2019_en/vue.config.js
@@ -0,0 +1,4 @@
+// vue.config.js
+module.exports = {
+    publicPath: "/en/budapest-2019/"
+}
